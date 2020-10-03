7af958c159c942070112d465fb52d3ae97eb3423
podiki <podiki@users.noreply.github.com>
2020-10-02 22:16:10 -0400

    machine dependent polybar, emacs, few tweaks for xmonad

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar bar1 &
+# Launch bar based on hostname
+case $HOSTNAME in
+    (azazel) polybar azazel &;;
+    (raven)  polybar raven &;;
+esac
 
 echo "Bars launched..."
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -2,3 +2,6 @@ export DEFAULT_USER=$USER
 #TERM='xterm-256color'
 
 export EDITOR='emacsclient -a nano'
+
+# colors from wpgtk (so running commands non-interactively get color too)
+(cat ~/.config/wpg/sequences &)

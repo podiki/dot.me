370ee7cb22814ae50410a3b27eeba321441839d3
podiki <podiki@users.noreply.github.com>
2024-08-02 13:01:16 -0400

    guix: manifests: desktop: update for rofi wayland

--- guix/.config/guix/manifests/desktop-manifest.scm
+++ guix/.config/guix/manifests/desktop-manifest.scm
@@ -4,86 +4,102 @@
 ;; capture the channels being used, as returned by "guix describe".
 ;; See the "Replicating Guix" section in the manual.
 
-(specifications->manifest
- '("rofi-calc"
-   "gnome-themes-extra" ; for some defaults
-   "adwaita-icon-theme" ; ditto
-   "breeze-icons"       ; and for KDE too
-   "hicolor-icon-theme" ; fallback
-   "arc-icon-theme"
-   "arc-theme"
-   "orchis-theme"
-   "papirus-icon-theme"
-   "firefox"
-   "signal-desktop"
-   "element-desktop"
-   "zoom"
-   "flatpak"
-   "xdg-desktop-portal-gtk"
-   "xdg-desktop-portal-wlr"
-   "font-abattis-cantarell"
-   "font-adobe-source-code-pro"
-   "font-adobe-source-sans-pro"
-   "font-adobe-source-serif-pro"
-   "font-google-noto"
-   "font-google-roboto"
-   "font-liberation"
-   "font-microsoft-web-core-fonts"
-   "thunar"
-   "lxsession" ; just for lxpolkit
-   "lxappearance"
-   "aspell-dict-en"
-   "aspell"
-   "darkman"
-   "dunst"
-   "xdg-utils"
-   "piper"
-   "bluez"
-   "blueman"
-   "openrgb"
-   "corectrl"
-   "lm-sensors"
-   "udiskie"
-   "xrdb"
-   "dex"
-   "v4l-utils:gui"
-   "geeqie"
-   "evince"
-   "libreoffice"
-   "heroic"
-   "steam"
-   "headsetcontrol"
-   "feh"
-   "mpv"
-   "opencl-icd-loader"
-   "rocm-opencl-runtime"
-   "darktable"
-   "xmodmap"
-   "xsettingsd"
-   "font-awesome"
-   "polybar"
-   "kitty"
-   "alacritty"
-   "foot"
-   "redshift:gtk"
-   "picom"
-   "python" ; for GUIX_PYTHONPATH needed for redshift
-   "syncthing"
-   "syncthing-gtk"
-   "pulseaudio"
-   "xset"
-   "xrandr"
-   "pavucontrol"
-   "pasystray"
-   "rofi-pass"
-   "rofi"
-   "yubikey-oath-dmenu"
-   "font-dejavu"
-   "arandr"
-   "xprop"
-   "wireplumber"
-   ;; wayland
-   ;"wlsunset"
-   "gammastep"
-   ;"waybar" ;once new version merged
-   ))
+(use-modules (guix transformations))
+
+(define transform2
+  (options->transformation
+    '((with-latest . "rofi-wayland"))))
+
+(define transform1
+  (options->transformation
+    '((with-latest . "rofi-wayland")
+      (with-input . "rofi=rofi-wayland"))))
+
+(concatenate-manifests
+ (list
+  (packages->manifest
+   (list (transform2 (specification->package "rofi-wayland"))
+         (transform1 (specification->package "rofi-calc"))))
+  (specifications->manifest
+   '("gnome-themes-extra" ; for some defaults
+     "adwaita-icon-theme" ; ditto
+     "breeze-icons"       ; and for KDE too
+     "hicolor-icon-theme" ; fallback
+     "arc-icon-theme"
+     "arc-theme"
+     "orchis-theme"
+     "papirus-icon-theme"
+     "firefox"
+     "signal-desktop"
+     "element-desktop"
+     "zoom"
+     "flatpak"
+     "xdg-desktop-portal-gtk"
+     "xdg-desktop-portal-wlr"
+     "xdg-desktop-portal-hyprland"
+     "font-abattis-cantarell"
+     "font-adobe-source-code-pro"
+     "font-adobe-source-sans-pro"
+     "font-adobe-source-serif-pro"
+     "font-google-noto"
+     "font-google-roboto"
+     "font-liberation"
+     "font-microsoft-web-core-fonts"
+     "thunar"
+     "lxsession" ; just for lxpolkit
+     "lxappearance"
+     "aspell-dict-en"
+     "aspell"
+     "darkman"
+     "dunst"
+     "xdg-utils"
+     "piper"
+     "bluez"
+     "blueman"
+     "openrgb"
+     "corectrl"
+     "lm-sensors"
+     "udiskie"
+     "xrdb"
+     "dex"
+     "v4l-utils:gui"
+     "geeqie"
+     "evince"
+     "libreoffice"
+     "heroic"
+     "steam"
+     "headsetcontrol"
+     "feh"
+     "mpv"
+     "opencl-icd-loader"
+     "rocm-opencl-runtime"
+     "darktable"
+     "xmodmap"
+     "xsettingsd"
+     "font-awesome"
+     "polybar"
+     "kitty"
+     "alacritty"
+     "foot"
+     "redshift:gtk"
+     "picom"
+     "python" ; for GUIX_PYTHONPATH needed for redshift
+     "syncthing"
+     "syncthing-gtk"
+     "pulseaudio"
+     "xset"
+     "xrandr"
+     "pavucontrol"
+     "pasystray"
+     "rofi-wayland"
+     "rofi-pass-wayland"
+     "yubikey-oath-dmenu"
+     "font-dejavu"
+     "arandr"
+     "xprop"
+     "wireplumber"
+     ;; wayland
+     "xremap-wlroots"
+     "gammastep"
+     "waybar"
+     "swaynotificationcenter"))))

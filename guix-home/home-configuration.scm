f66247ac7ea8b35fe783723f5d0065ed7f4dca7f
podiki <podiki@users.noreply.github.com>
2024-11-24 11:22:50 -0500

    guix: move packages from profiles to guix home

--- guix-home/home-configuration.scm
+++ guix-home/home-configuration.scm
@@ -8,11 +8,268 @@
              (gnu packages)
              (gnu services)
              (guix gexp)
+             (guix transformations)
              (gnu home services desktop)
              (gnu home services gnupg)
              (gnu home services shepherd)
              (gnu home services sound)
              (gnu packages gnupg))
+
+;; Package lists, with some inspiration from
+;; <https://git.sr.ht/~efraim/guix-config> (see item/efraim-home.scm).
+(define %cli-packages
+  (map specification->package+output
+       (list "python-tldr"
+             "python-yubikey-manager"
+             "colordiff"
+             "thefuck"
+             "curl"
+             "tree"
+             "btrfs-progs"
+             "compsize"
+             "autoconf"
+             "automake"
+             ;; "mu"
+             "make"
+             "lm-sensors"
+             "unzip"
+             "zsh"
+             "htop"
+             "stow"
+             "git"
+             "git:send-email"
+             "pwgen"
+             "password-store"
+             "openssh"
+             "pinentry"
+             "ccid"
+             "gnupg")))
+
+(define native-comp
+  (options->transformation
+   '((with-input . "emacs-minimal=emacs-pgtk"))))
+
+(define org-msg-git
+  (options->transformation
+   '((with-input . "emacs-minimal=emacs-pgtk")
+     (with-git-url
+      .
+      "emacs-org-msg=https://github.com/jeremy-compostella/org-msg"))))
+
+(define %emacs-packages
+  (append
+   (list (org-msg-git (specification->package "emacs-org-msg")))
+   (map native-comp
+        (map specification->package
+             (list "emacs-visual-fill-column"
+                   "emacs-goto-chg"
+                   "emacs-frames-only-mode"
+                   "emacs-paradox"
+                   "emacs-exec-path-from-shell"
+                   "emacs-color-theme-solarized"
+                   ;; commented out missing/not currently used
+                   ;; packages
+                   ;; "emacs-molokai-theme"
+                   "emacs-monokai-theme"
+                   "emacs-spacemacs-theme"
+                   "emacs-doom-themes"
+                   "emacs-solaire-mode"
+                   "emacs-mixed-pitch"
+                   ;; native comp fail "emacs-powerline"
+                   ;; native comp fail for powerline "emacs-spaceline-all-the-icons"
+                   "emacs-doom-modeline"
+                   "emacs-dashboard"
+                   "emacs-ido-completing-read+"
+                   "emacs-ido-vertical-mode"
+                   "emacs-smex"
+                   "emacs-multiple-cursors"
+                   "emacs-orderless"
+                   ;; "emacs-ivy"
+                   ;; "emacs-swiper"
+                   ;; "emacs-counsel"
+                   ;; "emacs-ivy-posframe"
+                   ;; "emacs-ivy-rich"
+                   ;; "emacs-counsel-org-clock"
+                   ;; "emacs-hydra"
+                   ;; "emacs-ivy-hydra"
+                   "emacs-consult"
+                   "emacs-which-key"
+                   ;; "emacs-color-identifiers-mode"
+                   "emacs-highlight-symbol"
+                   ;; "emacs-define-word"
+                   "emacs-langtool"
+                   "emacs-neotree"
+                   "emacs-diredfl"
+                   "emacs-dired-git-info"
+                   "emacs-org"
+                   "emacs-org-contrib"
+                   "emacs-org-superstar"
+                   "emacs-org-bullets"
+                   "emacs-org-modern"
+                   "emacs-org-appear"
+                   "emacs-org-present"
+                   "emacs-htmlize"
+                   "emacs-org-re-reveal"
+                   "emacs-ox-reveal"
+                   ;; "emacs-org2blog"
+                   ;; "emacs-org-gcal"
+                   "emacs-calfw"
+                   "emacs-org-super-agenda"
+                   "emacs-org-ref"
+                   "emacs-org-noter"
+                   ;; "emacs-advice-patch"
+                   "emacs-org-cliplink"
+                   "emacs-org-auto-tangle"
+                   ;; "emacs-org-msg"
+                   "emacs-mu4e-alert"
+                   ;; "emacs-mu4e-marker-icons"
+                   "emacs-magit"
+                   "emacs-company"
+                   "emacs-company-quickhelp"
+                   "emacs-company"
+                   "emacs-flycheck"
+                   ;; "emacs-flycheck-color-mode-line"
+                   "emacs-rainbow-delimiters"
+                   "emacs-smartparens"
+                   "emacs-slime"
+                   "emacs-slime-company"
+                   ;; "emacs-info-look"
+                   ;; "emacs-cython-mode"
+                   ;; "emacs-python-mode"
+                   ;; "emacs-company-jedi"
+                   ;; "emacs-haxe-mode"
+                   "emacs-eglot"
+                   "emacs-fountain-mode"
+                   "emacs-markdown-mode"
+                   "emacs-olivetti"
+                   "emacs-ledger-mode"
+                   "emacs-flycheck-ledger"
+                   "emacs-auctex"
+                   ;; "emacs-latex-pretty-symbols"
+                   "emacs-company-auctex"
+                   "emacs-company-math"
+                   "emacs-emms"
+                   "emacs-nerd-icons"
+                   "emacs-all-the-icons"
+                   "emacs-all-the-icons-dired"
+                   "emacs-pgtk"
+                   "emacs-pdf-tools"
+                   "emacs-use-package"
+                   "guile"
+                   "emacs-debbugs"
+                   "emacs-geiser"
+                   "emacs-geiser-guile"
+                   "emacs-guix"
+                   "emacs-yasnippet"
+                   "emacs-highlight-sexp"
+                   "emacs-ledger-mode"
+                   "emacs-vertico"
+                   "emacs-vertico-posframe"
+                   "emacs-marginalia"
+                   "python-lsp-server"
+                   "emacs-docker-compose-mode"
+                   "emacs-eat"
+                   ;; mail
+                   "mu"
+                   "oauth2ms"
+                   "isync"
+                   "go-gitlab.com-shackra-goimapnotify")))))
+
+(define rofi-calc-wayland
+  (options->transformation
+   '((with-input . "rofi=rofi-wayland"))))
+
+(define %desktop-packages
+  (append
+   (list (rofi-calc-wayland (specification->package "rofi-calc")))
+   (map specification->package+output
+        (list "gnome-themes-extra" ; for some defaults
+              "adwaita-icon-theme" ; ditto
+              "breeze-icons"       ; and for KDE too
+              "hicolor-icon-theme" ; fallback
+              "arc-icon-theme"
+              "arc-theme"
+              "orchis-theme"
+              "papirus-icon-theme"
+              "firefox"
+              "signal-desktop"
+              "element-desktop"
+              "zoom"
+              "flatpak"
+              "xdg-desktop-portal-gtk"
+              "xdg-desktop-portal-wlr"
+              "xdg-desktop-portal-hyprland"
+              ;; conflicts with mpv (but really shouldn't as it only
+              ;; propagates glib), among others; how to get gsettings
+              ;; then?
+              ;; "glib:bin"
+              ;; fonts
+              "font-abattis-cantarell"
+              "font-adobe-source-code-pro"
+              "font-adobe-source-sans-pro"
+              "font-adobe-source-serif-pro"
+              "font-google-noto"
+              "font-google-roboto"
+              "font-liberation"
+              "font-microsoft-web-core-fonts"
+              "thunar"
+              "lxsession" ; just for lxpolkit
+              "lxappearance"
+              "aspell-dict-en"
+              "aspell"
+              "darkman"
+              "dunst"
+              "xdg-utils"
+              "piper"
+              "bluez"
+              "blueman"
+              "openrgb"
+              "corectrl"
+              "lm-sensors"
+              "udiskie"
+              "xrdb"
+              "dex"
+              "v4l-utils:gui"
+              "geeqie"
+              "evince"
+              "libreoffice"
+              "heroic"
+              "steam"
+              "headsetcontrol"
+              "feh"
+              "mpv"
+              "opencl-icd-loader"
+              "rocm-opencl-runtime"
+              "darktable"
+              "xmodmap"
+              "xsettingsd"
+              "font-awesome"
+              "polybar"
+              "kitty"
+              "alacritty"
+              "foot"
+              "redshift:gtk"
+              "picom"
+              "python" ; for GUIX_PYTHONPATH needed for redshift
+              "syncthing"
+              "syncthing-gtk"
+              "pulseaudio"
+              "xset"
+              "xrandr"
+              "pavucontrol"
+              "pasystray"
+              "rofi-wayland"
+              "rofi-pass-wayland"
+              "yubikey-oath-dmenu"
+              "font-dejavu"
+              "arandr"
+              "xprop"
+              "wireplumber"
+              ;; wayland
+              "xremap-wlroots"
+              "gammastep"
+              "waybar"
+              "swaynotificationcenter"))))
 
 (define goimapnotify-gmail-service
   (shepherd-service
@@ -58,7 +315,7 @@
 (home-environment
   ;; Below is the list of packages that will show up in your
   ;; Home profile, under ~/.guix-home/profile.
-  ;;(packages (specifications->packages (list "heroic")))
+  (packages (append %cli-packages %emacs-packages %desktop-packages))
 
   ;; Below is the list of Home services.  To search for available
   ;; services, run 'guix home search KEYWORD' in a terminal.
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -14,47 +14,6 @@ source /etc/profile
 source ~/.profile
 # Guix Home:1 ends here
 
-# Guix profile loading
-# Test if on a Guix System to set the extra profiles variable and source them. Otherwise, set and source the default one on a foreign distribution. For Guix, also source anything in a profiles ~/etc/profile.d~ directory (while this an expected place, have only seen Flatpak put something in there on Guix).
-
-
-# [[file:README.org::*Guix profile loading][Guix profile loading:1]]
-if [[ -s /run/current-system/profile ]]; then
-    export GUIX_EXTRA_PROFILES="$HOME/.config/guix/profiles"
-    for i in $GUIX_EXTRA_PROFILES/*; do
-        profile=$i/$(basename "$i")
-        if [ -f "$profile"/etc/profile ]; then
-            GUIX_PROFILE="$profile"
-            . "$GUIX_PROFILE"/etc/profile
-        fi
-
-        if [ -d "$profile"/etc/profile.d/ ]; then
-            for j in "$profile"/etc/profile.d/*.sh ; do
-                if [ -r "$j" ]; then
-                    . $j
-                fi
-            done
-        fi
-
-        unset profile
-    done
-else
-    export GUIX_PROFILE="$HOME/.config/guix/current"
-    . "$GUIX_PROFILE/etc/profile"
-    #  export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
-fi
-# Guix profile loading:1 ends here
-
-# ~XDG_CONFIG_DIRs~ fix
-# To work around some search paths not being exported consistently in Guix, e.g. [[https://issues.guix.gnu.org/50103][issue #50103]], add the desktop profile to ~XDG_CONFIG_DIRS~
-
-
-# [[file:README.org::*~XDG_CONFIG_DIRs~ fix][~XDG_CONFIG_DIRs~ fix:1]]
-if [[ -s /run/current-system/profile ]]; then
-    export XDG_CONFIG_DIRS=$GUIX_EXTRA_PROFILES/desktop/desktop/etc/xdg:$XDG_CONFIG_DIRS
-fi
-# ~XDG_CONFIG_DIRs~ fix:1 ends here
-
 # Homebrew
 # Utilize homebrew if it exists (Mac only)
 

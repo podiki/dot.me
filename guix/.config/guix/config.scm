09f3a0b25a7869fb368ccf0390326dd7272479fc
podiki <podiki@users.noreply.github.com>
2026-01-14 21:38:06 -0500

    guix: clean up some cruft, indent properly
    used guix's dir-locals:
    <https://codeberg.org/guix/guix/src/branch/master/.dir-locals.el>

--- guix/.config/guix/config.scm
+++ guix/.config/guix/config.scm
@@ -1,6 +1,3 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
-
 (use-modules (gnu)
              (guix download) ;for url-fetch (udev rule)
              (guix packages) ;for origin (udev rule)
@@ -19,21 +16,19 @@
              (gnu packages admin) ;; for corectrl
              (gnu packages gnome)) ;; for libratbag (piper)
 
-(use-service-modules
- cups
- dbus
- desktop
- ;docker
- sddm
- linux
- mcron
- networking
- security-token ; for pcscd
- sound
- ssh
- syncthing
- sysctl
- xorg)
+(use-service-modules cups
+                     dbus
+                     desktop
+                     sddm
+                     linux
+                     mcron
+                     networking
+                     security-token ; for pcscd
+                     sound
+                     ssh
+                     syncthing
+                     sysctl
+                     xorg)
 
 (define fstrim-job
   ;; Run fstrim on all applicable mounted drives once a week
@@ -48,7 +43,8 @@
   #~(job "5 0 * * *"            ;Vixie cron syntax
          "guix gc -F 1G"))
 
-(define cachy-url "https://github.com/CachyOS/kernel-patches/raw/refs/heads/master/6.17/")
+(define cachy-url
+  "https://github.com/CachyOS/kernel-patches/raw/refs/heads/master/6.17/")
 
 (define (cachy-patch name hash)
   (origin
@@ -92,120 +88,115 @@
 (operating-system
   (locale "en_US.utf8")
   (timezone "America/New_York")
-  (keyboard-layout (keyboard-layout "us"
-                                    #:options '("ctrl:swapcaps")))
+  (keyboard-layout (keyboard-layout "us" #:options '("ctrl:swapcaps")))
   (host-name "narya")
-  (users (cons* (user-account
-                  (name "john")
-                  (comment "John")
-                  (group "users")
-                  (home-directory "/home/john")
-                  (shell (file-append zsh "/bin/zsh"))
-                  (supplementary-groups
-                   ;"docker" 
-                    '("wheel" "netdev" "audio" "video" "kvm")))
-                %base-user-accounts))
+  (users
+   (cons* (user-account
+            (name "john")
+            (comment "John")
+            (group "users")
+            (home-directory "/home/john")
+            (shell (file-append zsh "/bin/zsh"))
+            (supplementary-groups '("wheel" "netdev" "audio" "video" "kvm")))
+          %base-user-accounts))
   (packages
-    (append
-     (map specification->package '("xinitrc-xsession" "hyprland" "ntfs-3g"))
-      %base-packages))
-  (services (cons*
-             (service pam-limits-service-type
-               (list
-                 ;; higher open file limit, helpful for Wine and esync
-                 (pam-limits-entry "*" 'both 'nofile 524288)
-                 ;; lower nice limit for users, but root can go further to rescue system
-                 (pam-limits-entry "*" 'both 'nice -19)
-                 (pam-limits-entry "root" 'both 'nice -20)))
-             (service kernel-module-loader-service-type '("ntsync"))
-             (service pcscd-service-type)
-             (service peroxide-service-type) ;testing
-             (service bluetooth-service-type
-                      (bluetooth-configuration (fast-connectable? #t)
-                                               (privacy 'device)
-                                               (just-works-repairing 'always)))
-             (udev-rules-service 'u2f libfido2 #:groups '("plugdev"))
-             (udev-rules-service 'headsetcontrol headsetcontrol)
-             (udev-rules-service 'steam-devices steam-devices-udev-rules)
-             (udev-rules-service 'openrgb openrgb)
-             (simple-service 'blueman dbus-root-service-type (list blueman))
-             (simple-service 'ratbagd dbus-root-service-type (list libratbag))
-             (simple-service 'corectrl-polkit polkit-service-type
-                             (list corectrl))
-             ;; dbus files from corectrl are system-service files,
-             ;; so need to add to dbus-root-service
-             (simple-service 'corectrl-dbus dbus-root-service-type
-                             (list corectrl))
-             ;(geoclue-service)
-             ;; to have geoclue in the system profile, so the agent autostart file is visible
-             ;; (simple-service 'profile-geoclue profile-service-type
-             ;;                 (list (specification->package "geoclue")))
-             ;; fstrim seemed to be running at unexpected times and causing sleep problems
-             ;; disable for now (June 2022)
-             ;; (simple-service 'my-mcron-jobs
-             ;;                 mcron-service-type
-             ;;                 (list ; garbage-collector-job
-             ;;                       fstrim-job))
-             (service cups-service-type
-                      (cups-configuration
-                       (web-interface? #t)))
-             (service syncthing-service-type
-                      (syncthing-configuration (user "john")))
-             ;(service sddm-service-type)
-             (set-xorg-configuration (xorg-configuration (extra-config
-                                                          '("Section \"Device\"
+   (append
+    (map specification->package '("xinitrc-xsession" "hyprland" "ntfs-3g"))
+    %base-packages))
+  (services
+   (cons*
+    (service
+     pam-limits-service-type
+     (list
+      ;; higher open file limit, helpful for Wine and esync
+      (pam-limits-entry "*" 'both 'nofile 524288)
+      ;; lower nice limit for users, but root can go further to rescue system
+      (pam-limits-entry "*" 'both 'nice -19)
+      (pam-limits-entry "root" 'both 'nice -20)))
+    (service kernel-module-loader-service-type '("ntsync"))
+    (service pcscd-service-type)
+    (service peroxide-service-type) ;testing
+    (service
+     bluetooth-service-type
+     (bluetooth-configuration (fast-connectable? #t)
+                              (privacy 'device)
+                              (just-works-repairing 'always)))
+    (udev-rules-service 'u2f libfido2 #:groups '("plugdev"))
+    (udev-rules-service 'headsetcontrol headsetcontrol)
+    (udev-rules-service 'steam-devices steam-devices-udev-rules)
+    (udev-rules-service 'openrgb openrgb)
+    (simple-service 'blueman dbus-root-service-type (list blueman))
+    (simple-service 'ratbagd dbus-root-service-type (list libratbag))
+    (simple-service 'corectrl-polkit polkit-service-type (list corectrl))
+    ;; dbus files from corectrl are system-service files,
+    ;; so need to add to dbus-root-service
+    (simple-service 'corectrl-dbus dbus-root-service-type (list corectrl))
+    ;;(geoclue-service)
+    ;; to have geoclue in the system profile, so the agent autostart file is visible
+    ;; (simple-service 'profile-geoclue profile-service-type
+    ;;                 (list (specification->package "geoclue")))
+    ;; fstrim seemed to be running at unexpected times and causing sleep problems
+    ;; disable for now (June 2022)
+    ;; (simple-service 'my-mcron-jobs
+    ;;                 mcron-service-type
+    ;;                 (list ; garbage-collector-job
+    ;;                       fstrim-job))
+    (service cups-service-type
+             (cups-configuration
+               (web-interface? #t)))
+    (service syncthing-service-type
+             (syncthing-configuration (user "john")))
+    (set-xorg-configuration
+     (xorg-configuration (extra-config
+                          '("Section \"Device\"
                                                                Identifier \"device-amdgpu\"
                                                                Driver \"amdgpu\"
                                                                Option \"SWCursor\" \"on\"
                                                              EndSection")))
-                                     gdm-service-type)
-             ;(service docker-service-type)
-             (modify-services %desktop-services
-                              ;(delete gdm-service-type) ; replaced by lightdm
-                              ;; don't use USB modems, scanners, or network-manager-applet
-                              (delete modem-manager-service-type)
-                              (delete usb-modeswitch-service-type)
-                              (delete sane-service-type)
-                              ;; not sure how to
-                              ;(delete (specification->package "network-manager-applet"))
-                              ;; ignore the power key since a certain naughty kitten likes to press it
-                              (elogind-service-type config =>
-                                                    (elogind-configuration
-                                                     (inherit config)
-                                                     (handle-power-key 'ignore)))
-                              ;; Set the default sample rate for the pulseaudio daemon to be
-                              ;; 48000; needed for the Valve Index mic, see
-                              ;; https://github.com/ValveSoftware/SteamVR-for-Linux/issues/215#issuecomment-526791835
-                              ;; (pulseaudio-service-type config =>
-                              ;;                          (pulseaudio-configuration
-                              ;;                           (daemon-conf '((default-sample-rate . 44100)))))
-                              (delete pulseaudio-service-type)
-                              (guix-service-type config =>
-                                                 (guix-configuration
-                                                  (inherit config)
-                                                  (substitute-urls
-                                                   ;; Reverse the order to put Bordeaux first, adding in the US mirror
-                                                   (reverse (append '("https://substitutes.nonguix.org"
-                                                                      "https://cache-cdn.guix.moe")
-                                                           %default-substitute-urls
-                                                           '("https://bordeaux-us-east-mirror.cbaines.net/"))))
-                                                  (authorized-keys
-                                                   (append (list (local-file "substitutes.nonguix.org.pub")
-                                                                 (plain-file "guix-moe.pub"
-                                                                             "(public-key (ecc (curve Ed25519) (q #552F670D5005D7EB6ACF05284A1066E52156B51D75DE3EBD3030CD046675D543#)))"))
-                                                           %default-authorized-guix-keys))))
-                              (sysctl-service-type config =>
-                                (sysctl-configuration
-                                 (settings
-                                  (append '(;; see <https://www.phoronix.com/news/Linux-Splitlock-Hurts-Gaming>
-                                            ("kernel.split_lock_mitigate" . "0")
-                                            ("vm.swappiness" . "10")
-                                            ;; potential performance
-                                            ;; or stability with some
-                                            ;; games and now a default
-                                            ;; in e.g. Arch
-                                            ("vm.max_map_count" . "1048576"))
-                                          %default-sysctl-settings)))))))
+     gdm-service-type)
+    (modify-services %desktop-services
+      ;; don't use USB modems, scanners, or network-manager-applet
+      (delete modem-manager-service-type)
+      (delete usb-modeswitch-service-type)
+      (delete sane-service-type)
+      ;; not sure how to
+      ;;(delete (specification->package "network-manager-applet"))
+      ;; ignore the power key since a certain naughty kitten likes to press it
+      (elogind-service-type
+       config => (elogind-configuration (inherit config)
+                                        (handle-power-key 'ignore)))
+      ;; Set the default sample rate for the pulseaudio daemon to be 48000;
+      ;; needed for the Valve Index mic, see
+      ;; <https://github.com/ValveSoftware/SteamVR-for-Linux/issues/215#issuecomment-526791835>
+      (delete pulseaudio-service-type)
+      (guix-service-type
+       config =>
+       (guix-configuration
+         (inherit config)
+         (substitute-urls
+          ;; Reverse the order to put Bordeaux first, adding in the US mirror
+          (reverse (append '("https://substitutes.nonguix.org"
+                             "https://cache-cdn.guix.moe")
+                           %default-substitute-urls
+                           '("https://bordeaux-us-east-mirror.cbaines.net/"))))
+         (authorized-keys
+          (append
+           (list
+            (local-file "substitutes.nonguix.org.pub")
+            (plain-file "guix-moe.pub"
+                        "(public-key (ecc (curve Ed25519) (q #552F670D5005D7EB6ACF05284A1066E52156B51D75DE3EBD3030CD046675D543#)))"))
+           %default-authorized-guix-keys))))
+      (sysctl-service-type
+       config =>
+       (sysctl-configuration
+         (settings
+          (append '(;; see <https://www.phoronix.com/news/Linux-Splitlock-Hurts-Gaming>
+                    ("kernel.split_lock_mitigate" . "0")
+                    ("vm.swappiness" . "10")
+                    ;; potential performance or stability with some games and
+                    ;; now a default in e.g. Arch
+                    ("vm.max_map_count" . "1048576"))
+                  %default-sysctl-settings)))))))
   (kernel linux-vr-mod)
   (kernel-loadable-modules (list v4l2loopback-linux-module))
   (kernel-arguments
@@ -217,66 +208,69 @@
      "amdgpu.ppfeaturemask=0xffffffff"
      "amd_pstate=active"))
   (initrd microcode-initrd)
-  (firmware (cons* amdgpu-firmware
-                   realtek-firmware
-                   %base-firmware))
+  (firmware
+   (cons* amdgpu-firmware
+          realtek-firmware
+          %base-firmware))
   ;; Use the UEFI variant of GRUB with the EFI System
   ;; Partition mounted on /boot/efi.
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets '("/boot/efi"))
-                (keyboard-layout keyboard-layout)))
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (targets '("/boot/efi"))
+      (keyboard-layout keyboard-layout)))
 
-  (file-systems (append
-                 (list (file-system
-                         (device (file-system-label "system-new"))
-                         (mount-point "/")
-                         (type "btrfs")
-                         (flags '(no-atime))
-                         (options "subvol=root,compress=zstd,ssd"))
-                       (file-system
-                         (device (file-system-label "system-new"))
-                         (mount-point "/swap")
-                         (type "btrfs")
-                         (flags '(no-atime))
-                         (options "subvol=swap,ssd"))
-                       (file-system
-                         (device (file-system-label "system-new"))
-                         (mount-point "/gnu/store")
-                         (type "btrfs")
-                         (flags '(no-atime))
-                         (options "subvol=gnu-store,compress=zstd,ssd"))
-                       (file-system
-                         (device (file-system-label "system-new"))
-                         (mount-point "/var/log")
-                         (type "btrfs")
-                         (flags '(no-atime))
-                         (options "subvol=var-log,compress=zstd,ssd"))
-                       (file-system
-                         (device (file-system-label "system-new"))
-                         (mount-point "/home")
-                         (type "btrfs")
-                         (flags '(no-atime))
-                         (options "subvol=home,compress=zstd,ssd"))
-                       (file-system
-                         (device (uuid "8149-982B" 'fat))
-                         (mount-point "/boot/efi")
-                         (type "vfat"))
-                       (file-system
-                         (device (file-system-label "extra"))
-                         (mount-point "/mnt/extra")
-                         (type "ext4")
-                         (flags '(no-atime)))
-                       (file-system
-                         (device (file-system-label "nvmind"))
-                         (mount-point "/mnt/nvmind")
-                         (type "ext4")
-                         (flags '(no-atime))))
-                 %base-file-systems))
+  (file-systems
+   (append
+    (list (file-system
+            (device (file-system-label "system-new"))
+            (mount-point "/")
+            (type "btrfs")
+            (flags '(no-atime))
+            (options "subvol=root,compress=zstd,ssd"))
+          (file-system
+            (device (file-system-label "system-new"))
+            (mount-point "/swap")
+            (type "btrfs")
+            (flags '(no-atime))
+            (options "subvol=swap,ssd"))
+          (file-system
+            (device (file-system-label "system-new"))
+            (mount-point "/gnu/store")
+            (type "btrfs")
+            (flags '(no-atime))
+            (options "subvol=gnu-store,compress=zstd,ssd"))
+          (file-system
+            (device (file-system-label "system-new"))
+            (mount-point "/var/log")
+            (type "btrfs")
+            (flags '(no-atime))
+            (options "subvol=var-log,compress=zstd,ssd"))
+          (file-system
+            (device (file-system-label "system-new"))
+            (mount-point "/home")
+            (type "btrfs")
+            (flags '(no-atime))
+            (options "subvol=home,compress=zstd,ssd"))
+          (file-system
+            (device (uuid "8149-982B" 'fat))
+            (mount-point "/boot/efi")
+            (type "vfat"))
+          (file-system
+            (device (file-system-label "extra"))
+            (mount-point "/mnt/extra")
+            (type "ext4")
+            (flags '(no-atime)))
+          (file-system
+            (device (file-system-label "nvmind"))
+            (mount-point "/mnt/nvmind")
+            (type "ext4")
+            (flags '(no-atime))))
+    %base-file-systems))
 
   (swap-devices
    (list
     (swap-space
-     (target "/swap/swapfile")
-     (dependencies (filter (file-system-mount-point-predicate "/swap")
-                           file-systems))))))
+      (target "/swap/swapfile")
+      (dependencies (filter (file-system-mount-point-predicate "/swap")
+                            file-systems))))))
